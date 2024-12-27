0662ee91cefe1fa3af18c08718a991623cb799d1
Abu Sayed <sayedcsekuet@gmail.com>
2024-12-27 11:39:31 +0100

    Adding api key authentication

--- src/app_module.py
+++ src/app_module.py
@@ -1,19 +1,29 @@
-from nest.core import PyNestFactory, Module
+from typing import Tuple, List
 
-from .auth.auth_bearer import JWTBearer
-from .config import config, ANONYMIZE_ROUTES
+from fastapi.security import HTTPBearer, APIKeyHeader
+from nest.core import PyNestFactory, Module
+from starlette.authentication import BaseUser, SimpleUser
+from starlette.middleware.authentication import AuthenticationMiddleware
+from .config import ANONYMIZE_ROUTES
 from .app_controller import AppController
 from .app_service import AppService
 from src.users.users_module import UsersModule
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+from .auth.auth_bearer_token_backend import AuthBearerTokenBackend
+from .auth.auth_api_key_backend import AuthApiKeyBackend
 
 
-@Module(imports=[UsersModule], controllers=[AppController], providers=[AppService])
+@Module(
+    imports=[UsersModule],
+    controllers=[AppController],
+    providers=[AppService],
+)
 class AppModule:
     pass
 
+
+api_key_name = 'x-api-key'
 
 app = PyNestFactory.create(
     AppModule,
@@ -21,16 +31,12 @@ app = PyNestFactory.create(
     title="PyNest Application",
     version="1.0.0",
     debug=True,
-    dependencies=[Depends(JWTBearer(exclude_routes=ANONYMIZE_ROUTES))]
+    dependencies=[Depends(HTTPBearer(auto_error=False)), Depends(APIKeyHeader(name=api_key_name, auto_error=False))]
 )
 http_server = app.get_server()
 origins = [
-    "http://localhost:3000",
-    "localhost:3000",
-    "http://localhost:3001",
-    "localhost:3001",
+    "*",
 ]
-
 app.use(
     CORSMiddleware,
     allow_origins=origins,
@@ -38,6 +44,8 @@ app.use(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.use(AuthenticationMiddleware, backend=AuthBearerTokenBackend(exclude_routes=ANONYMIZE_ROUTES))
+app.use(AuthenticationMiddleware, backend=AuthApiKeyBackend(name=api_key_name, exclude_routes=ANONYMIZE_ROUTES))
 
 
 @http_server.on_event("startup")
